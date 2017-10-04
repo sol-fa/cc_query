623379925d69164af33adafbeed024758d849015
sol-fa <taku.ando.work@gmail.com>
2017-10-04 23:06:15 +0900

    create cc_query.js

---
+++ cc_query.js
@@ -0,0 +1,23 @@
+!function(window) {
+
+  var cc = function( element ) {
+
+    // 文字列以外はfalseを返す
+    if( typeof element !== 'string' ) {
+
+      return false;
+    }
+
+    // インスタンスを生成
+    return new cc.prototype.init( element );
+  }
+
+  calc.prototype = {
+
+    // コンストラクタ
+    init: function( element ) {
+
+      this._element = num;
+    }
+  }
+}
